f32ccee579b6d7b477c139976b0525790a2c0cb3
Aaron Imbrock <aimbrock@gmail.com>
2023-02-07 23:47:25 -0800

    http resposne works

--- server.py
+++ server.py
@@ -2,11 +2,24 @@
 
 import socket
 
-s = socket.socket()
+# create a socket object of address type IPv4 and TCP (defaults)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(("0.0.0.0", 8080))
 s.listen(10) # queue depth, a required param set to an arbitrary value of 10
 
-# wait for a connection
-conn, address = s.accept()
-print(str(conn.recv(4096)))
+http_response = b"""HTTP/1.1 200 OK\r
+CONTENT-Length: 8\r
+
+HOLA!\n\r\n"""
+
+while True:
+    # wait for a connection
+    clientsocket, address = s.accept()
+    print(f"Connection from {address}")
+    clientsocket.send(http_response)
+    clientsocket.shutdown(socket.SHUT_WR)
+    clientsocket.close()
+
+s.close()
 # conn.send ...
